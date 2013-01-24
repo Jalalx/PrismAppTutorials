dc401128eca1ac9a8a10c47ec7cfbc8d44bee62e
Jalal <sjamini@gmail.com>
2013-01-24 21:02:07 +0330

    Step 1

---
+++ PrismApp.Shell/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace PrismApp.Shell
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterType<MainWindow>();
+        }
+
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            var mainWindow = Container.Resolve<MainWindow>();
+            mainWindow.Show();
+            return mainWindow;
+        }
+    }
+}
