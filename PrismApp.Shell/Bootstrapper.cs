5ba88c1bdb61e42f7f540d1f97099f91db57a9f1
Jalal <sjamini@gmail.com>
2013-01-26 04:59:39 +0330

    Step 2 done

--- PrismApp.Shell/Bootstrapper.cs
+++ PrismApp.Shell/Bootstrapper.cs
@@ -1,21 +1,43 @@
-﻿using Microsoft.Practices.Prism.UnityExtensions;
+﻿using System;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
 using Microsoft.Practices.Unity;
 
 namespace PrismApp.Shell
 {
     public class Bootstrapper : UnityBootstrapper
     {
+        protected override Microsoft.Practices.Prism.Logging.ILoggerFacade CreateLogger()
+        {
+            return new TextFileLogger();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new DirectoryModuleCatalog()
+            {
+                ModulePath = Environment.CurrentDirectory
+            };
+        }
+
         protected override void ConfigureContainer()
         {
+            // Register default core services
             base.ConfigureContainer();
+            // Register Shell (main window)
             Container.RegisterType<MainWindow>();
         }
 
         protected override System.Windows.DependencyObject CreateShell()
         {
-            var mainWindow = Container.Resolve<MainWindow>();
-            mainWindow.Show();
-            return mainWindow;
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
         }
     }
 }
---
+++ PrismApp.Shell/TextFileLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Practices.Prism.Logging;
+
+namespace PrismApp.Shell
+{
+    public class TextFileLogger : ILoggerFacade
+    {
+        private readonly string _fileName = Path.Combine(Environment.CurrentDirectory, "PrismApp.log");
+        private readonly object _syncLock = new object();
+
+        public TextFileLogger()
+        {
+        }
+
+        public void Log(string message, Category category, Priority priority)
+        {
+            WriteToFile(category.ToString(), message);
+        }
+
+
+        private void WriteToFile(string type, string message)
+        {
+            lock (_syncLock)
+            {
+                using (var writer = new StreamWriter(_fileName, true, Encoding.UTF8))
+                {
+                    writer.WriteLine(string.Format("{0:G}\t{1}:{2}", DateTime.Now, type, message));
+                }
+            }
+        }
+    }
+}
